a90b8e950e31b0a5dd20a77fb8637b9ad1743063
ion.dormenco <ion.dormenco@maxcode.net>
2020-05-22 13:42:46 +0300

    Update AssemblyInfo.cs

--- Polybool.Net/Properties/AssemblyInfo.cs
+++ Polybool.Net/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Polybool.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Boolean operations on polygons (union, intersection, difference, xor) (this library is a port for .net of polybooljs")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Polybool.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
